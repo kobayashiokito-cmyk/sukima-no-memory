a1ffe882bee9a341ba3a6223289bcbdb7eb194d9
SUKIMA <kobayashi.okito@gmail.com>
2026-04-16 19:05:26 +0900

    fix login url null handling

--- client/src/const.ts
+++ client/src/const.ts
@@ -7,9 +7,11 @@ export const getLoginUrl = () => {
   const redirectUri = `${window.location.origin}/api/oauth/callback`;
   const state = btoa(redirectUri);
 
-  const url = oauthPortalUrl
-  ? new URL(`${oauthPortalUrl}/app-auth`)
-  : null;
+  if (!oauthPortalUrl || !appId) {
+    return "#";
+  }
+
+  const url = new URL(`${oauthPortalUrl}/app-auth`);
   url.searchParams.set("appId", appId);
   url.searchParams.set("redirectUri", redirectUri);
   url.searchParams.set("state", state);
